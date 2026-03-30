9649ed3b7b476ff1ed086ca9af1091dea69fed89
anaconda <anaconda@sjt418scope007>
2026-03-30 16:13:12 +0530

    First commit

---
+++ src/main/java/com/mycompany/adadlabcat_sam/ADADLabCAT_SAM.java
@@ -0,0 +1,11 @@
+package com.mycompany.adadlabcat_sam;
+
+
+public class ADADLabCAT_SAM {
+
+    public static void main(String[] args) {
+        System.out.println("Samriddhi Sharma");
+        System.out.println("23MIC0097");
+        System.out.println("Jenkins Build Successful");
+    }
+}
